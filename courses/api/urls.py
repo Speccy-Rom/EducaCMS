9f80663e487ddd3cbb68e6fe47e8be573d2b8f3f
Speccy-rom <speccy.rom@gmail.com>
2021-02-18 23:32:24 +0300

    создал набор обработчиков для модели Course

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
+from rest_framework import routers
 from . import views
 
 
 app_name = 'courses'
 
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
 
 urlpatterns = [
     path('subjects/',
@@ -12,5 +15,6 @@ urlpatterns = [
     path('subjects/<pk>/',
          views.SubjectDetailView.as_view(),
          name='subject_detail'),
+    path('', include(router.urls)),
 
 ]
--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,12 @@
 from rest_framework import generics
 from ..models import Subject
 from .serializers import SubjectSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from ..models import Course
+from rest_framework import viewsets
+from .serializers import CourseSerializer
 
 
 class SubjectListView(generics.ListAPIView):
@@ -13,3 +19,9 @@ class SubjectDetailView(generics.RetrieveAPIView):
     """"""
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    """блок обработчика"""
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
