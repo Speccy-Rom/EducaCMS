fc98a0691d8a230dfdd072ff6112d450a59ad585
Speccy-rom <speccy.rom@gmail.com>
2021-02-19 00:22:24 +0300

    создал RESTful API для взаимодействия приложения с другими системами

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from ..models import Subject
-from ..models import Course, Module
+from ..models import Course, Module, Content
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -27,3 +27,32 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = ['id', 'subject', 'title', 'slug', 'overview',
                   'created', 'owner', 'modules']
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug',
+                  'overview', 'created', 'owner', 'modules']
--- courses/api/views.py
+++ courses/api/views.py
@@ -1,12 +1,12 @@
-from rest_framework import generics
-from ..models import Subject
-from .serializers import SubjectSerializer
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
+from rest_framework import generics, viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from ..models import Subject, Course
 from rest_framework.response import Response
-from ..models import Course
-from rest_framework import viewsets
-from .serializers import CourseSerializer
+from .serializers import CourseSerializer, SubjectSerializer, CourseWithContentsSerializer
+from rest_framework.decorators import action
+from .permissions import IsEnrolled
 
 
 class SubjectListView(generics.ListAPIView):
@@ -25,3 +25,26 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     """блок обработчика"""
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @action(methods=['post'],
+            detail=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        """
+
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+
+    @action(methods=['get'],
+            detail=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated,
+                                IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
